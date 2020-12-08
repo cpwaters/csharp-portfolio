93ee2bfb4bb8f70de65791ded7c10ae9f4f7b71e
Chris Waters <chris-waters@sandhills.com>
2020-12-08 09:32:14 +0000

    New program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lottery Program");
+        }
+    }
+}
